6ef44f6a2de639cb0104021179f4f7b470341f3f
Kemeshat <kemeshat@usf.edu>
2026-03-08 22:33:30 -0400

    made changes to cc_7.js

--- cc_7.js
+++ cc_7.js
@@ -4,9 +4,11 @@ const button = document.getElementById('updateButton');
 const ctaHeadline = document.getElementById('ctaHeadline');
 
 // Update CTA on button click
-button.addEventListener('click', () => {
-  if(input.value.trim() !== ''){
-    ctaHeadline.textContent = input.value + " 🍳";
-    input.value = '';
-  }
-});
+if (button) {
+  button.addEventListener('click', () => {
+    if(input.value.trim() !== ''){
+      ctaHeadline.textContent = input.value + " 🍳";
+      input.value = '';
+    }
+  });
+}
